d0353d9517ffcc3051d33086fa778f3c92a57d7c
nodoka13 <nodoka13@gmail.com>
2024-06-13 11:54:03 +0900

    Topbar, Sidebar, Footer 리셋

--- src/Footerr.js
+++ src/Footerr.js
@@ -3,7 +3,8 @@ function Footerr(){
         <footer class="sticky-footer bg-white">
         <div class="container my-auto">
             <div class="copyright text-center my-auto">
-                <span>Copyright &copy; Your Website 2020</span>
+                <span>
+                </span>
             </div>
         </div>
     </footer>
