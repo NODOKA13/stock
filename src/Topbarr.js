e20215746e574dc29c4e3f94c02c73eafcc36d4a
nodoka13 <nodoka13@gmail.com>
2024-06-14 00:53:04 +0900

    로그인 이동까지만

--- src/Topbarr.js
+++ src/Topbarr.js
@@ -1,13 +1,22 @@
-function Topbarr(){
-    return(
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-            
-            {/*<!-- Sidebar Toggle (Topbar)-->*/}
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                <i class="fa fa-bars"></i>
-            </button>
+import { Link } from "react-router-dom";
 
+function Topbarr() {
+    const linkStyle = {
+        fontFamily: "'Font Awesome 5 Free'",
+    };
 
+    return (
+        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <div className="container-fluid">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item flex-fill">
+                        <div className="bg-gradient-primary p-4 font-medium text-white text-sm rounded d-flex align-items-center">
+                            <i className="fas fa-user-circle fa-lg mr-2"></i>
+                            <Link to="/login" style={linkStyle} className="text-white mx-3">Login</Link>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
